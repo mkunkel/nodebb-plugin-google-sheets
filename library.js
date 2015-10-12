b353b80418587f7c19b1092615bba3c1a03eda9c
tailot <tailot@gmail.com>
2015-10-12 15:50:00 +0200

    Regex: added pre-compiled support

--- library.js
+++ library.js
@@ -2,15 +2,15 @@
     "use strict";
 
     var Gforms = {},
-    	embed = '<iframe src="https://docs.google.com/forms/d/$1/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>';
-    var regularUrl = /<a href="(?:https?:\/\/)?(?:docs\.google\.com)\/forms\/d\/(.+)\/viewform.*>.+<\/a>/g;
+        embed = '<iframe src="https://docs.google.com/forms/d/$1/viewform?$2" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>',
+        regularUrl = /<a href="(?:https?:\/\/)?(?:docs\.google\.com)\/forms\/d\/(.+)\/viewform\?*(.*)>.+<\/a>/g;
     Gforms.parse = function(data, callback) {
-		if (!data || !data.postData || !data.postData.content) {
-			return callback(null, data);
-		}
-       	if (data.postData.content.match(regularUrl)) {
-			data.postData.content = data.postData.content.replace(regularUrl, embed);
-		}
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(regularUrl)) {
+            data.postData.content = data.postData.content.replace(regularUrl, embed);
+        }
         callback(null, data);
     };
     module.exports = Gforms;
