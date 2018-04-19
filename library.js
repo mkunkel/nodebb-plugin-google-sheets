691c44db246919b099912fe17920c6606d8f0a11
Mike Kunkel <mike@mkunkel.com>
2018-04-19 11:56:31 -0500

    Add link to sheet
    Allow user to open sheet in a new tab

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var Gsheets = {},
-        embed = '<iframe src="https://docs.google.com/spreadsheets/d/$1/edit?usp=sharing" class="sheets-embed">Loading...</iframe>',
+        embed = '<iframe src="https://docs.google.com/spreadsheets/d/$1/edit?usp=sharing" class="sheets-embed">Loading...</iframe><br><a href="https://docs.google.com/spreadsheets/d/$1/edit?usp=sharing" target="_blank">Open sheet in new tab</a>',
         regularUrl = /<a href="(?:https?:\/\/)?(?:docs\.google\.com)\/spreadsheets\/d\/(.+)\/edit?.*>.+<\/a>/g;
     Gsheets.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
